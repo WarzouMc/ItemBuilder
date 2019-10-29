34cce733b90dbce7474849a97fc5452db11c2953
WarzouMc <49000416+WarzouMc@users.noreply.github.com>
2019-10-29 17:36:57 +0100

    Update ItemBuilder.java

--- ItemBuilder.java
+++ ItemBuilder.java
@@ -1,14 +1,20 @@
-package fr.WarzouMc.SkyExpanderInternalPlugin.utils.itemBuilder;
+package fr.WarzouMc.SkyExpanderInternalPlugin.utils.graphics.itemBuilder;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * <i>Just open ItemBuilder</i>
@@ -22,6 +28,7 @@ public class ItemBuilder {
     private Inventory inventory;
     private ItemStack itemStack;
     private ItemMeta itemMeta;
+    private SkullMeta skullMeta;
 
     /**
      * init ItemBuilder
@@ -48,6 +55,54 @@ public class ItemBuilder {
      */
     public ItemBuilder(Material material, int amount, int data) {
         this.itemStack = new ItemStack(material, amount, (byte)data);
+    }
+
+    /**
+     * init ItemBuiler
+     * @param itemStack
+     */
+    public ItemBuilder(ItemStack itemStack){
+        this.itemStack = itemStack;
+    }
+
+    /**
+     * Set item
+     * @param material
+     * @return
+     */
+    public ItemBuilder setItem(Material material){
+        this.itemStack.setType(material);
+        return this;
+    }
+
+    /**
+     * Set amount
+     * @param amount
+     * @return
+     */
+    public ItemBuilder setAmount(int amount){
+        this.itemStack.setAmount(amount);
+        return this;
+    }
+
+    /**
+     * Set data
+     * @param data
+     * @return
+     */
+    public ItemBuilder setData(int data){
+        this.itemStack = new ItemStack(itemStack.getType(), itemStack.getAmount(), (byte)data);
+        return this;
+    }
+
+    /**
+     * Set ItemStack
+     * @param itemStack
+     * @return
+     */
+    public ItemBuilder setItemStack(ItemStack itemStack){
+        this.itemStack = itemStack;
+        return this;
     }
 
     /**
@@ -109,6 +164,28 @@ public class ItemBuilder {
     }
 
     /**
+     * hide enchant
+     * @return
+     */
+    public ItemBuilder hideEnchante(){
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    /**
+     * show enchant
+     * @return
+     */
+    public ItemBuilder showEnchant(){
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        itemMeta.removeItemFlags(ItemFlag.HIDE_ENCHANTS);
+        itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    /**
      * Set durrability of item
      * /!\ 100 >= percent >= 0
      * @param percent
@@ -121,6 +198,23 @@ public class ItemBuilder {
             return this;
         }
         itemStack.setDurability((short) (itemStack.getDurability() * (percent / 100)));
+        return this;
+    }
+
+    public ItemBuilder setSkullTexture(String value){
+        this.skullMeta = (SkullMeta) itemStack.getItemMeta();
+        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), null);
+        gameProfile.getProperties().put("textures", new Property("textures", value));
+
+        try{
+            Field gameProfileField = skullMeta.getClass().getDeclaredField("profile");
+            gameProfileField.setAccessible(true);
+            gameProfileField.set(skullMeta, gameProfile);
+        } catch (IllegalAccessException | NoSuchFieldException error) {
+            error.printStackTrace();
+        }
+
+        itemStack.setItemMeta(skullMeta);
         return this;
     }
 
